1e7dc03bbfefb4e206e23200e505e6bffb502e71
stevenlee87 <stevenlee87@126.com>
2017-01-04 22:58:54 +0800

    edit day2/names.py

--- s14/day2/names.py
+++ s14/day2/names.py
@@ -1,7 +1,9 @@
+import copy
+
 __author__ = "Steven Lee"
 
 # names = "Steven Lee Jack Bob"
-
+'''
 names = ["Stevenlee", "Lee", "Jack", "Bob"]
 
 print("1,", names)  # ['Stevenlee', 'Lee', 'Jack', 'Bob']
@@ -29,10 +31,37 @@ print("9,", names.count("Tom"))  # 查找列表中某个字串符的数量
 
 names.reverse()
 print("10,", names)  # 列表反转
+'''
 
-names2 = names.copy()
+names = ["Stevenlee", "Lee", "Jack", "Bob", "jerry"]
+print(names[:])
+print(names[0:-1])  # print ['Stevenlee', 'Lee', 'Jack']
+print(names[0:-1:2])
+print(names[::2])
+
+
+'''names = ["Stevenlee", "Lee", "Jack", "Bob", ["1", "2", "3"]]
+# names2 = names.copy()  # shallow copy
+# names2 = copy.copy(names)  # shallow copy
+names2 = copy.deepcopy(names)  # deep copy
 print(names)
 print(names2)
 names[0] = "jerry"
+names[4][0] = "5"
 print(names)
 print(names2)
+'''
+
+person = ['name', ['saving', 100]]
+'''
+p1 = copy.copy(person)
+p2 = person[:]
+p3 = list(person)
+'''
+p1 = person[:]
+p2 = person[:]
+
+p1[0] = 'stevenlee'
+p2[0] = 'jane'
+print(p1)
+print(p2)
