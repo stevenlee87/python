813128472e7985713c9272639c5a74cf5b46b793
stevenlee87 <imepisode1@gmail.com>
2017-01-04 18:06:36 +0800

    add encode_decode.py and names.py

---
+++ s14/day2/encode_decode.py
@@ -0,0 +1,7 @@
+__author__ = "Steven Lee"
+
+msg = "我爱北京天安门"
+
+print(msg)
+print(msg.encode())
+print(msg.encode(encoding="utf-8").decode(encoding="utf-8"))
---
+++ s14/day2/names.py
@@ -0,0 +1,31 @@
+__author__ = "Steven Lee"
+
+# names = "Steven Lee Jack Bob"
+
+names = ["Stevenlee", "Lee", "Jack", "Bob"]
+
+print("1,", names)  # ['Stevenlee', 'Lee', 'Jack', 'Bob']
+print("2,", names[0])  # Stevenlee
+print("3,", names[-2:])  # ['Jack', 'Bob']
+
+names.append("Michael")
+print("4,", names)  # ['Stevenlee', 'Lee', 'Jack', 'Bob', 'Michael']
+
+names.insert(1, "Tom")  # 插入新字段
+print("5,", names)  # ['Stevenlee', 'Tom', 'Lee', 'Jack', 'Bob', 'Michael']
+
+# delete 3种方法
+names.pop(2)  # or names.pop(-4)
+# del names[2]
+# names.remove("Lee")
+print("6,", names)
+
+print("7,", names.index("Stevenlee"))
+
+names.append("Tom")
+print("8,", names)
+
+print("9,", names.count("Tom"))  # 查找列表中某个字串符的数量
+
+names.reverse()
+print("10,", names)  # 列表反转
