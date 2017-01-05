66c683323161e4bf973a1c526ac0ae88b715ddd3
stevenlee87 <stevenlee87@126.com>
2017-01-05 21:56:56 +0800

    add shopping_cart.py

--- s14/day2/shopping_cart.py
+++ s14/day2/shopping_cart.py
@@ -2,19 +2,25 @@ __author__ = "Steven Lee"
 
 goods_list = [["1", "iphone", "5800"], ["2", "mac", "12000"], ["3", "starbuck coffee", "31"], ["4", "python book", "81"]
     , ["5", "bike", "800"]]
+goods_num = []
+shopping_cart = []
+salary = input("Please input your salary:")
 
-print(goods_list[:][0])
-
-'''
 while True:
-    prompt_salary = input("Please input your salary:")
+    print("your account balance is ", salary)
     print("goods list is:", goods_list)
 
-    prompt_buy = input("Please input which goods(number) you want to buy:")
+    buy = input("Please input which goods(number) you want to buy:")
+    if str(buy) == "Q" or str(buy) == "q":
+        print("Out of your shopping trip!")
+        break
     for i in goods_list:
-        goods_number_list.append(i[0])
-'''
-
-#prompt_buy = input("Please input which goods number you want to buy:")
-#prompt_exit = input("Q")
-
+        if buy == i[0] and int(salary) > int(i[2]):
+            shopping_cart.append(i)
+            salary = int(salary)
+            salary -= int(i[2])
+            print("added {_goods} to you shopping cart!".format(_goods=i))
+            continue
+        else:
+            print("Your balance is not enough to buy goods ", i)
+            continue
