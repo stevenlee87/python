928fda29aa556eecdf3b552e033a7a7fb8da2eb3
stevenlee87 <imepisode1@gmail.com>
2017-01-05 18:46:26 +0800

    delete .idea and add shopping_cart.py

---
+++ s14/day2/shopping_cart.py
@@ -0,0 +1,20 @@
+__author__ = "Steven Lee"
+
+goods_list = [["1", "iphone", "5800"], ["2", "mac", "12000"], ["3", "starbuck coffee", "31"], ["4", "python book", "81"]
+    , ["5", "bike", "800"]]
+
+print(goods_list[:][0])
+
+'''
+while True:
+    prompt_salary = input("Please input your salary:")
+    print("goods list is:", goods_list)
+
+    prompt_buy = input("Please input which goods(number) you want to buy:")
+    for i in goods_list:
+        goods_number_list.append(i[0])
+'''
+
+#prompt_buy = input("Please input which goods number you want to buy:")
+#prompt_exit = input("Q")
+
