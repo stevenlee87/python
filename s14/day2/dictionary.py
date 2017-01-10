ca7b812f19163a38cff55ac4020d255f7a954b74
stevenlee87 <imepisode1@gmail.com>
2017-01-10 12:05:04 +0800

    add dictionary.py

---
+++ s14/day2/dictionary.py
@@ -0,0 +1,11 @@
+__author__ = "Steven Lee"
+
+info = {
+    'stu1101': 'jack li',
+    'stu1102': 'bob wang',
+    'stu1103': 'Tom lu'
+}
+
+print(info)
+info['stu1101'] = '杰克'
+print(info['stu1101'])
