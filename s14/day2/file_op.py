4faeb3b488a36d53d65c9d3bf41b69ab86630737
stevenlee87 <stevenlee87@126.com>
2017-01-14 22:32:40 +0800

    edit string.py

--- s14/day2/file_op.py
+++ s14/day2/file_op.py
@@ -1,7 +1,9 @@
 __author__ = "Steven Lee"
 
 # data = open('yesterday', encoding='utf-8').read()
-f = open('yesterday', encoding='utf-8')  # 文件句柄(文件的大小，文件的起始位置)
-f.readlines()
-
+# f = open('yesterday', 'r', encoding='utf-8')  # 文件句柄(文件的大小，文件的起始位置)
+f = open('yesterday2', 'a', encoding='utf-8')  # 文件句柄(文件的大小，文件的起始位置)
+# data = f.read()
+# print(data)
+f.write("\nhello world!\n")
 f.close()
