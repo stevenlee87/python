12d2dde12675d91eccfb4c32111c657e3a08d19e
stevenlee87 <stevenlee87@126.com>
2017-01-14 23:20:42 +0800

    edit file_op.py

--- s14/day2/file_op.py
+++ s14/day2/file_op.py
@@ -1,9 +1,19 @@
 __author__ = "Steven Lee"
 
 # data = open('yesterday', encoding='utf-8').read()
-# f = open('yesterday', 'r', encoding='utf-8')  # 文件句柄(文件的大小，文件的起始位置)
-f = open('yesterday2', 'a', encoding='utf-8')  # 文件句柄(文件的大小，文件的起始位置)
+f = open('yesterday', 'r', encoding='utf-8')  # 文件句柄(文件的大小，文件的起始位置)
+# f = open('yesterday2', 'a', encoding='utf-8')  # 文件句柄(文件的大小，文件的起始位置)
 # data = f.read()
 # print(data)
-f.write("\nhello world!\n")
+# f.write("\nhello world!\n")
+
+print(f.tell())
+print(f.readline()) # 可以指定读入的字符数
+print(f.readline())
+print(f.readline())
+print(f.tell())
+f.seek(0)
+print(f.readline())
+
+print(f.encoding)
 f.close()
