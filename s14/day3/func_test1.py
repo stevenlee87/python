6418e9890a4ec53ab8f5421f0bc182083424bdad
stevenlee87 <imepisode1@gmail.com>
2017-01-20 18:50:26 +0800

    add day3/func_test1.py

---
+++ s14/day3/func_test1.py
@@ -0,0 +1,16 @@
+__author__ = "Steven Lee"
+
+
+def func1():  # 函数
+    """this is func1"""
+    print("this is func1")
+    return 0
+
+
+def func2():  # 过程
+    """this is func2"""
+    print("this is func2")
+
+x = func1()  #
+y = func2()  # 过程就是没有返回值的函数
+
