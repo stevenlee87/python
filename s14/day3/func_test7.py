02351f733d1e9b6bab434f998400ef4ba493afbe
stevenlee87 <stevenlee87@126.com>
2017-01-28 19:56:54 +0800

    edit func_test7.py

--- s14/day3/func_test7.py
+++ s14/day3/func_test7.py
@@ -2,6 +2,9 @@ __author__ = "Steven Lee"
 
 
 def test(*args):
+    """
+    *args：接收N个位置参数，比如1, 2, 3, 4, 5和*[1, 2, 3, 4, 5]
+    """
     print(args)
 
 test(1, 2, 3, 4, 5)
@@ -20,5 +23,21 @@ def test2(**kwargs):
     **kwargs:把N个关键字参数，转换成字典的方式
     """
     print(kwargs)
+    print(kwargs['name'])
+    print(kwargs['age'])
+    print(kwargs['sex'])
 
 test2(name='steven', age='20', sex='male')  # print {'name': 'steven', 'age': '20', 'sex': 'male'}
+test2(**{'name': 'stevenlee', 'age': 9, 'sex': 'male'})  # print{'name': 'stevenlee', 'age': 9, 'sex': 'male'}
+
+print('------------')
+
+
+def test3(name, age=18, **kwargs):
+    print(name)
+    print(age)
+    print(kwargs)
+
+test3('ste', 3, sex='male')
+test3('ste', sex='male', age=20)
+test3('steven')
