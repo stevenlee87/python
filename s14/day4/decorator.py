e1e5fdcebc70e7a7793b7c4bc5fa29cd0586d222
stevenlee87 <imepisode1@gmail.com>
2017-02-13 21:16:23 +0800

    add days4/decorator.py

---
+++ s14/day4/decorator.py
@@ -0,0 +1,30 @@
+__author__ = "Steven Lee"
+
+import time
+
+# 装饰器
+# 定义：("器"就是函数的意思)：本质是函数(装饰其他函数),就是为其他函数添加附加功能
+# 原则：1.不能修改被装饰的函数的源代码
+#       2.不能修改被装饰的函数的调用方式
+
+# 实现装饰器知识储备：
+# 1.函数即变量
+# 2.高阶函数
+# 3.嵌套函数
+# 高阶函数+嵌套函数=>>装饰器
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time=time.time()
+        func()
+        stop_time=time.time()
+        print('the func run time is %s' %(stop_time-start_time))
+    return wrapper
+
+@timer
+def test1():
+    time.sleep(3)
+    print('in the test1')
+
+test1()
+
