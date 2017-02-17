2855d4a2a3a3f846b5231302675359e4c19b362b
stevenlee87 <imepisode1@gmail.com>
2017-02-17 18:14:33 +0800

    edit days4/decorator2.py and day3/high_order_func.py

--- s14/day3/high_order_func.py
+++ s14/day3/high_order_func.py
@@ -1,4 +1,6 @@
 __author__ = "Steven Lee"
+# a.把一个函数名当做实参传给另外一个函数
+# b.返回值中包含函数名
 
 
 def add(a, b, f):
--- s14/day4/decorator2.py
+++ s14/day4/decorator2.py
@@ -1,13 +1,29 @@
+import time
 __author__ = "Steven Lee"
 # 函数就是一种“变量”
+# a.把一个函数名当做实参传给另外一个函数
+# b.返回值中包含函数名
+
+# def foo():
+#     print('in the foo')
+#     bar()
+#
+#
+# def bar():
+#     print('in the bar')
+#
+# foo()
 
 
-def foo():
-    print('in the foo')
-    bar()
+def timer(func):
+    start_time = time.time()
+    func()
+    stop_time = time.time()
+    print('the func run time is %s' % (stop_time-start_time))  # stop_time减去start_time
 
 
-def bar():
-    print('in the bar')
+def test1():
+    time.sleep(3)
+    print('in the test1')
 
-foo()
+timer(test1)  # 改变了函数的调用方式
