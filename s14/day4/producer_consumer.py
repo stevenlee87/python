46960d76d1f25b6e8c7b9b39873d3ce238169d0a
stevenlee87 <imepisode1@gmail.com>
2017-03-03 10:19:20 +0800

    edit days4/producer_consumer.py

--- s14/day4/producer_consumer.py
+++ s14/day4/producer_consumer.py
@@ -1,14 +1,17 @@
 import time
+
 __author__ = "Steven Lee"
+
+
 # 协程
 
 
 def consumer(name):
     print("%s 准备吃包子啦!" % name)
     while True:
-       baozi = yield
+        baozi = yield
 
-       print("包子[%s]来了,被[%s]吃了!" %(baozi,name))
+        print("包子[%s]来了,被[%s]吃了!" % (baozi, name))
 
 
 def producer(name):
@@ -19,8 +22,9 @@ def producer(name):
     print("开始准备做包子啦!")
     for i in range(10):
         time.sleep(1)
-        print("做了1个包子,分成了两半!")
+        print("%s做了1个包子,分成了两半!" % name)
         c.send(i)
         c2.send(i)
+
 
 producer("alex")
