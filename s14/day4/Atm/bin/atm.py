777209291d6097d94d7d725f3c030544cdcf8cd9
stevenlee87 <imepisode1@gmail.com>
2017-03-21 18:46:45 +0800

    edit day4/Atm project

--- s14/day4/Atm/bin/atm.py
+++ s14/day4/Atm/bin/atm.py
@@ -2,8 +2,9 @@ import os
 import sys
 __author__ = "Steven Lee"
 
-# print(os.path.dirname(os.path.abspath(__file__)))
-# print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(os.path.abspath(__file__))
+print(os.path.dirname(os.path.abspath(__file__)))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
