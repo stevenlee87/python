0ac5717290f0f5cb6208f020e52148ec558dba04
stevenlee87 <imepisode1@gmail.com>
2017-05-08 19:25:20 +0800

    edit day5/shelve_test.py

---
+++ s14/day5/shelve_test.py
@@ -0,0 +1,30 @@
+import shelve
+# import datetime
+__author__ = "Steven Lee"
+# shelve模块是一个简单的k,v将内存数据通过文件持久化的模块，可以持久化任何pickle可支持的python数据格式
+
+d = shelve.open('shelve_test')  # 打开一个文件
+print(d.get("name"))
+print(d.get("info"))
+print(d.get("date"))
+
+# info = {'age': 22, 'job': 'it'}
+# name = ["alex", "rain", "test"]
+# d["name"] = name  # 持久化列表
+# d["info"] = info  # 持久dict
+# d['date'] = datetime.datetime.now()
+# d.close()
+
+# class Test(object):
+#     def __init__(self, n):
+#         self.n = n
+#
+# t = Test(123)
+# t2 = Test(123334)
+#
+# name = ["alex", "rain", "test"]
+# d["test"] = name  # 持久化列表
+# d["t1"] = t  # 持久化类
+# d["t2"] = t2
+#
+# d.close()
