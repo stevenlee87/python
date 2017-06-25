d9e2712ccd0c38b8ff3f656e110b00fff592ba16
stevenlee87 <stevenlee87@126.com>
2017-06-25 23:48:22 +0800

    edit shutil_test.py

--- s14/day5/shutil_test.py
+++ s14/day5/shutil_test.py
@@ -1,3 +1,10 @@
+import shutil
 __author__ = "Steven Lee"
 
 # shutil 模块：高级的文件、文件夹、压缩包 处理模块
+
+# f1 = open("sys_test.py", encoding="utf-8")
+# f2 = open("copy_sys_test.py", "w", encoding="utf-8")
+# shutil.copyfileobj(f1, f2)
+
+shutil.copyfile("sys_test.py", "copy_sys_test2.py")
