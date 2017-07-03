a7e8ed79e65ed91888a7c9629f8acbb8ec2fa5cf
stevenlee87 <imepisode1@gmail.com>
2017-07-03 18:30:13 +0800

    edit day5\xml_module.py

--- s14/day5/xml_module.py
+++ s14/day5/xml_module.py
@@ -8,10 +8,27 @@ print(root.tag)
 
 # 遍历xml文档
 for child in root:
-    print(child.tag, child.attrib)
+    print(child.tag, "attrib", child.attrib)
     for i in child:
+        # print("tag:", i.tag, "text:", i.text)
         print(i.tag, i.text)
 
 # 只遍历year 节点
 for node in root.iter('year'):
     print(node.tag, node.text)
+
+# 修改
+for node in root.iter('year'):
+    new_year = int(node.text) + 1
+    node.text = str(new_year)
+    node.set("updated", "yes")
+
+tree.write("xml_test.xml")
+
+# 删除node
+for country in root.findall('country'):
+    rank = int(country.find('rank').text)
+    if rank > 50:
+        root.remove(country)
+
+tree.write('output.xml')
